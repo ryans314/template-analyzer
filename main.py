407208e57678d250e921bb6bdc0f00ca4e1ff1ff
Ryan Stam <ryans314@mit.edu>
2025-07-08 14:51:46 -0400

    feat: implement html parser

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from bs4 import BeautifulSoup, Tag
 
 from typing import List, Tuple, Any
 
@@ -35,14 +36,24 @@ def get_filepaths(path: str) -> List[str]:
     # elif (os.path.isdir(path)):
 
 
-#TODO: strengthen spec
-def parse_html(file_path: str) -> Any:
+def parse_html(content: str, file_path: str) -> Any:
     """
-    Given a filepath, return a useful data representation
-    containing information about the tags and class sets
-    in the html
+    Given a file_path and its content, return a 3-tuple for each tag in the file:
+    (tag_name, class_list (sorted), file_path)
     """
-    pass
+    soup = BeautifulSoup(content, "html.parser")
+    all_elements = soup.find_all(["div"])
+    tags = [elem for elem in all_elements if isinstance(elem, Tag)]
+
+    data = []
+    for tag in tags:
+        name = tag.name
+        classes = sorted(tag.attrs["class"])
+        classes.sort()
+        
+        data.append((name, classes, file_path))
+
+    return data
 
 def parse_data(data: dict) -> None:
     """
@@ -66,21 +77,32 @@ def aggregate_data() -> None:
 def format_aggregated_data() -> None:
     pass
 if __name__ == "__main__":
+
+    #parse command line arguments
     args = sys.argv
     if len(args) != 2:
-        print("Error: please provide a filepath or directory to analyze as a command-line argument.")
+        print("Error: please provide a file path or directory to analyze as a command-line argument.")
         sys.exit(1)
     path = args[1]
     
-    #parse command line arguments
-    files = get_filepaths(path)
-    print("h1")
-    #generate list of files
+    file_paths = get_filepaths(path)
 
-    #for each file:
+    data = []
+    for file_path in file_paths:
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                content = f.read()
+        except FileNotFoundError:
+            print(f"Error: the file \'{file_path}\' was not found")
+            sys.exit(1)
+        except IOError as e:
+            print(f"Error reading file: {e}")
+            sys.exit(1)
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            sys.exit(1)
 
-        # html -> data
-        # data -> db
+        data += parse_html(content, file_path)
 
     # aggregate data from db
 
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import pytest
 from pytest import raises
-from main import get_filepaths
+from main import get_filepaths, parse_html
 
+#  ------------ Tests for get_filepath -------------
 def test_get_filepaths_works_for_dir() -> None:
     result = get_filepaths("test_data")
     assert len(result) == 4
@@ -23,3 +24,38 @@ def test_get_filepaths_raises_error_for_invalid_path() -> None:
     with raises(FileNotFoundError):
         get_filepaths("additional_files")
 
+#  ------------ Tests for parse_html -------------
+
+def test_parse_html_has_correct_format() -> None:
+    html_str = "<div class='class-name'></div>"
+    data = parse_html(html_str, "folder/file.html")
+    assert len(data) == 1
+    div_data = data[0]
+    assert len(div_data) == 3
+    assert div_data == ("div", ["class-name"], "folder/file.html")
+
+def test_parse_html_sorts_class_names() -> None:
+    html_str = \
+    "<div class='align-center w-auto py-5'></div>" \
+    "<div class='w-auto align-center py-5'></div>"
+    data = parse_html(html_str, "folder/file.html")
+    assert len(data) == 2
+    div1_data = data[0]
+    div2_data = data[1]
+    
+    expected = ("div", ["align-center", "py-5", "w-auto"], "folder/file.html")
+    assert div1_data == expected
+    assert div2_data == expected
+
+def test_parse_html_works_on_nested_elements() -> None:
+    html_str = \
+    "<div class='align-center w-auto py-5'>" \
+        "<div class=\"sm:text-sm md:text-lg\"><p></p></div>" \
+    "</div>"
+    data = parse_html(html_str, "folder/file.html")
+    assert len(data) == 2 #should not include p
+    
+    expected_outer = ("div", ["align-center", "py-5", "w-auto"], "folder/file.html")
+    assert expected_outer in data
+    expected_inner = ("div", ["md:text-lg", "sm:text-sm"], "folder/file.html")
+    assert expected_inner in data
